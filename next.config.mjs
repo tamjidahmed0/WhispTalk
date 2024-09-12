3cd07aebb78d0d9ab66d177fd5004130d3766056
tamjid-ahmed <tamjidahmed644@gmail.com>
2024-09-12 23:40:14 +0600

    fix image loading on server

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,12 @@
 const nextConfig = {
     images: {
         remotePatterns: [
-          {
-            protocol: 'http',
-            hostname: 'localhost',
-            pathname: '**',
-            port:'1000'
-          },
+          // {
+          //   protocol: 'http',
+          //   hostname: 'localhost',
+          //   pathname: '**',
+          //   port:'1000'
+          // },
           {
             protocol: 'https',
             hostname: process.env.NEXT_PUBLIC_API,
